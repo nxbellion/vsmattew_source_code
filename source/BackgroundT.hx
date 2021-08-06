e2701fc1e11140924e56d5e2f091094a3d055f21
Neb <88507349+nxbellion@users.noreply.github.com>
2021-08-06 22:16:48 +0200

    Add files via upload

---
+++ source/BackgroundT.hx
@@ -0,0 +1,31 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.graphics.frames.FlxAtlasFrames;
+
+class BackgroundT extends FlxSprite
+{
+	public function new(x:Float, y:Float)
+	{
+		super(x, y);
+
+		frames = Paths.getSparrowAtlas("totally_not_tom");
+		animation.addByIndices('danceLeft', 'tom idle animation istanza 1', [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], "", 24, false);
+		animation.addByIndices('danceRight', 'tom idle animation istanza 1', [15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29], "", 24, false);
+
+		antialiasing = true;
+		animation.play('danceLeft');
+	}
+
+	var danceDir:Bool = false;
+
+	public function dance():Void
+	{
+		danceDir = !danceDir;
+
+		if (danceDir)
+			animation.play('danceRight', true);
+		else
+			animation.play('danceLeft', true);
+	}
+}
